0a3ed370bb68d8d1eed66d044bbfc8c86054a517
abhiv-97 <v.abhishek@outlook.com>
2022-06-04 17:51:57 +0100

    prob3 change

--- prob3.py
+++ prob3.py
@@ -12,8 +12,20 @@ def largest_prime_factor(num):
             #print(num, i)
     return num
 
+def prime_factors(num): #modified the method to return a list of factors instead
+    i = 2
+    factors = []
+    while i * i <= num: #looking for factors so stop when i^2 is greater than n
+        if num % i:     #if there is a modulus, move onto next integer
+            i+=1
+        else:
+            num //= i  
+            factors.append(i)
+    if n > 1:
+        factors.append(num) #add the last prime, left out by the previous loop
 
+    return factors
 
 if __name__ == "__main__":
     print(largest_prime_factor(n))
-    
+    print(prime_factors(n))   
